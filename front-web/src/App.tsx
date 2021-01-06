6bb83aba55c83d74c212e71128dad058959313f3
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2021-01-06 14:10:14 -0400

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
-
-import Navbar from './NavBar/index';
+import Home from './Home';
+import Navbar from './NavBar';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
